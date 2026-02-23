850c894d4c15b342a121e74073715194b16890e0
Huy Vo <voquanghuy225@gmail.com>
2026-02-23 17:17:34 +0700

    deploy static fe for website

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  // Thay 'afes_FE' bằng đúng tên repository của bạn trên GitHub
-  base: '/afes_FE/',
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  return {
+    // Nếu đang chạy build để quăng lên GitHub thì dùng '/afes_FE/', còn chạy local thì dùng '/'
+    base: command === 'dist' ? '/afes_FE/' : '/',
+    plugins: [react()],
+  }
 })
